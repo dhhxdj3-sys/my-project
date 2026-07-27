0fff6583a5a25e921626fc063a17609487094d1b
dhhxdj3-sys <dhhxdj3@gmail.com>
2026-07-27 19:16:45 +0800

    完成评论点赞以及收藏功能

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,9 +1,33 @@
 <script setup>
+import {ref} from 'vue'
+const commentText = ref('')
+
+const emit = defineEmits([
+    "like",
+    "collect",
+    "comment"
+])
 const props = defineProps({
     title:String,
     date:String,
-    desc:String
+    desc:String,
+    like:Number,
+    collect:Boolean,
+    comments:Array
 })
+function addLike(){
+    emit("like")
+}
+function addCollect(){
+    emit("collect")
+}
+function addComment(){
+    if(!commentText.value.trim()){
+        return
+    }
+    emit("comment",commentText.value)
+    commentText.value=""
+}
 </script>
 <template>
     <div class="card">
@@ -16,5 +40,31 @@ const props = defineProps({
                 <p>
                     {{ desc }}
                     </p>
+                    <button @click="addLike">点赞
+                        {{ like}}
+                    </button>
+                    <br>
+                  <button @click="addCollect">
+                    {{ collect ? "已收藏" : "收藏"}}
+                    </button>
                     </div>
+                    <div>
+                        <input 
+                        v-model="commentText"
+                        placeholder="请输入你的评论"
+                        
+                        >
+                        <button @click="addComment">
+                            发送
+                            </button>
+                            </div>
+                            <div>
+                                <p 
+                                v-for="item in comments"
+                                :key="item"
+                                
+                                >
+                                {{ item }}
+                                </p>
+                                </div>
     </template>
--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,26 +1,40 @@
 <script setup>
 import BlogCard from '../components/BlogCard.vue'
-const blogs = [{
+import {ref} from 'vue'
+
+
+const blogs =ref( [{
     title:'vue3组件',
     date:'2026-06-18',
-    desc:"学习props,emit"
+    desc:"学习props,emit",
+    like:0,
+    collect:false,
+    comments:[]
 },
 {
     title:'vue3计算属性',
     date:'2026-06-21',
-    desc:"学习computed"
+    desc:"学习computed",
+    like:0,
+    collect:false,
+    comments:[]
 }
 
-
-
-]
+])
 </script>
 <template>
     <BlogCard
-    v-for="blog in blogs"
+    v-for="(blog,index) in blogs"
+    :key="index"
     :title="blog.title"
     :date="blog.date"
     :desc="blog.desc"
+    :like="blog.like"
+    :collect="blog.collect"
+    :comments="blog.comments"
+    @like="blog.like++"
+    @collect="blog.collect=!blog.collect"
+    @comment="blog.comments.push($event)"
     
     
      />
